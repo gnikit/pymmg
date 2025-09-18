206e082e7493e56d1c79d07ffc5bbe9132d58f3a
gnikit <giannis.nikiteas@gmail.com>
2025-09-18 23:41:01 +0100

    chore: pre-commit fixes

--- src/pymmg/mmgs/__init__.py
+++ src/pymmg/mmgs/__init__.py
@@ -5,6 +5,7 @@ from pymmg.general import intercept_io_and_run
 
 
 def main():
+    """Entry point to mmgs binary."""
     binary = Path(__file__).parent / "mmgs"
     args = sys.argv[1:]
     intercept_io_and_run(binary, args)
